afbf5d62d5183d848242d063f0f971bb31b2563c
Henrique Nascimento <henriquenascandrade@gmail.com>
2023-05-01 01:46:28 -0300

    change user informations - session data

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,1 +1,9 @@
 <?= $this->extend('Layout/template') ?>
+
+<?= $this->section('content') ?>
+<div class="page-inner">
+    <div class="page-header">
+        <h4 class="page-title">Dashboard</h4>
+    </div>
+</div>
+<?= $this->endSection() ?>
