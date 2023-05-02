368f05ceb233c6f574d0c4d9ec0d094c76a0e1d0
Henrique Nascimento <henriquenascandrade@gmail.com>
2023-05-01 21:35:52 -0300

    fix user profile images upload and gender select and datepicker data user

---
+++ app/Views/user/profile.php
@@ -0,0 +1,106 @@
+<?= $this->extend('Layout/template') ?>
+
+<?= $this->section('content') ?>
+<div class="page-inner">
+    <h4 class="page-title">Seu Perfil</h4>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-with-nav">
+                <div class="card-header">
+                    <div class="row row-nav-line">
+                        <ul class="nav nav-tabs nav-line nav-color-secondary" role="tablist">
+                            <li class="nav-item"> <a class="nav-link active show" data-toggle="tab" href="#home" role="tab" aria-selected="true">Perfil</a> </li>
+                            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#profile" role="tab" aria-selected="false">Arquivos</a> </li>
+                            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#settings" role="tab" aria-selected="false">---</a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <form action="<?= site_url('user/update') ?>" method="post" class="card-body">
+                    <input type="hidden" id="id" name="id" value="<?= $user->id ?>">
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <div class="form-group form-group-default">
+                                <label>Nome</label>
+                                <input type="text" class="form-control" name="name" placeholder="Name" value="<?= $user->name ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group form-group-default">
+                                <label>E-mail</label>
+                                <input type="email" class="form-control" name="email" placeholder="Name" value="<?= $user->email ?>" disabled>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-4">
+                            <div class="form-group form-group-default">
+                                <label>Data de Nascimento</label>
+                                <input type="date" class="form-control" name="date_of_birth" id="datepicker" value="<?= date('Y-m-d', strtotime($user->date_of_birth)) ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group form-group-default">
+                                <label>Gênero</label>
+                                <select name="gender" class="form-control" id="gender">
+                                    <option value="">Selecione</option>
+                                    <option value="1" <?= $user->gender == 1 ? 'selected' : '' ?>>Masculino</option>
+                                    <option value="2" <?= $user->gender == 2 ? 'selected' : '' ?>>Feminino</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group form-group-default">
+                                <label>Telefone</label>
+                                <input type="text" class="form-control" value="<?= $user->phone ?>" name="phone" placeholder="Phone">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <div class="form-group form-group-default">
+                                <label>Senha</label>
+                                <input id="password" type="password" class="form-control" value="" placeholder="Nova senha..." name="password">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group form-group-default">
+                                <label>Confirmar senha</label>
+                                <input id="password" type="password" class="form-control" value="" placeholder="Confirmar nova senha...">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-right mt-3 mb-3">
+                        <button class="btn btn-success">salvar alterações</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-profile card-secondary">
+                <div class="card-header" style="background-image: url('../assets/img/blogpost.jpg')">
+                    <div class="profile-picture">
+                        <div class="avatar avatar-xl">
+                            <img src="<?= base_url().'uploads/users/images/' ?><?= $user->avatar ?>" alt="..." class="avatar-img rounded-circle">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="user-profile text-center">
+                        <div class="name"><?= $user->name ?></div>
+                        <div class="email"><?= $user->email ?></div>
+                    </div>
+                    <br>
+                    <div class="view-profile">
+                        <span><?= session()->getFlashdata('success') ?></span>
+                        <form action="<?= site_url('user/uploadImage/') ?><?= $user->id ?>" method="post" enctype="multipart/form-data">
+                            <input type="file" name="profile_image" class="file">
+                            <button class="btn btn-secondary btn-block">Alterar foto</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
