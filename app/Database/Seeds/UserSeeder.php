a5997a6c547bc44219ecf1f2fabf433df41ee1c9
Henrique Nascimento <henriquenascandrade@gmail.com>
2023-05-05 07:44:38 -0300

    add default image avatar

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -13,7 +13,8 @@ class UserSeeder extends Seeder
         $user = [
             'name'      => 'ADM',
             'email'     => 'admin@c4m.com',
-            'password'  => password_hash('12345678', PASSWORD_BCRYPT)
+            'password'  => password_hash('12345678', PASSWORD_BCRYPT),
+            'avatar' => 'default-avatar.png'
         ];
 
         $userModel->insert($user);
