7b5c50c9e76c351723b4941100ca6718de1e5abf
Henrique Nascimento <henriquenascandrade@gmail.com>
2023-05-15 07:45:27 -0300

    change get data array to object

--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -10,7 +10,7 @@ class ClientModel extends Model
     protected $table            = 'clients';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
+    protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
     protected $allowedFields    = [];
